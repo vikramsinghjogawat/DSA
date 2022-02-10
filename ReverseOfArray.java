b5ee3436b469e3c4cb2a71432bf2f984080b57f4
vikramsinghjogawat <vikramsinghjogawat5@gmail.com>
2022-02-11 02:24:55 +0530

    Add files via upload

---
+++ ReverseOfArray.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        //To reverse this array
+        int n = arr.length;
+        int temp;
+        for(int i=0;i<n/2;i++){
+            temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-i-1] =temp;
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+        
+    }
+}
