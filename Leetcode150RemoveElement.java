31c73981a6d762d2856d940536c75f3e5441f527
Vikram Singh Jogawat <vikramsinghjogawat5@gmail.com>
2026-07-08 21:39:20 +0530

    Added comment and package statement

--- Leetcode150RemoveElement.java
+++ Leetcode150RemoveElement.java
@@ -1,25 +1,28 @@
+package com.easybytes.springsecsection1.DSA;
+
 public class Leetcode150RemoveElement{
     public int removeElement(int[] nums, int val) {
-        List<Integer> l = new ArrayList<>();
+        List<Integer> indicesOfVal = new ArrayList<>();
         int i=0;
         while(i<nums.length){
             if(nums[i]==val){
-                l.add(i);
+                indicesOfVal.add(i);
             }
             i++;
         }
-        System.out.println("Number of vals "+l.size()+" - "+l);
-        if(l.size()==0) return nums.length;
+        System.out.println("Number of vals "+indicesOfVal.size()+" - "+indicesOfVal);
+        if(indicesOfVal.size()==0) return nums.length;
         i=nums.length-1;
         int count=0;
         while(i>=0){
             if(nums[i]!=val){
-                nums[l.get(count)]=nums[i];
+                //Starting from back if we find an element that is not val -> we put that value at the index where val was found
+                nums[indicesOfVal.get(count)]=nums[i];
                 count++;
             }
-            if(count==l.size()) break;
+            if(count==indicesOfVal.size()) break;
             i--;
         }
-        return nums.length-l.size();
+        return nums.length-indicesOfVal.size();
     }
 }
