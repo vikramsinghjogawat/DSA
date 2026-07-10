ba5e276a371168a92ef5510e75f807d543764207
Vikram Singh Jogawat <vikramsinghjogawat5@gmail.com>
2026-07-11 01:48:54 +0530

    Refactor package and implement reverseBits method

--- bitManupulation/Leetcode150ReverseBits.java
+++ bitManupulation/Leetcode150ReverseBits.java
@@ -1,5 +1,13 @@
-package bitManupulation;
+package com.easybytes.springsecsection1.DSA.bitManupulation;
 
 public class Leetcode150ReverseBits {
-
+  public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n = n >> 1;
+        }
+        return res;
+    }
 }
